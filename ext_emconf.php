a5ee594dca23b479d369c50d36aa1ee17e9f57ab
tieupmedia <hwt3@gmx.de>
2018-02-12 18:29:59 +0100

    [TASK] Compatibility changes in EM configuration for TYPO3 8.x

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,13 +32,17 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '0.0.6',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2.0-7.6.99',
-            'php' => '5.3.0-7.0.99',
+			'typo3' => '7.6.0-8.7.99',
+            'php' => '5.4.0-7.2.99',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:11:{s:9:"ChangeLog";s:4:"80be";s:12:"ext_icon.gif";s:4:"1bdc";s:14:"ext_tables.php";s:4:"d440";s:10:"README.txt";s:4:"ee2d";s:19:"doc/wizard_form.dat";s:4:"4688";s:20:"doc/wizard_form.html";s:4:"c8db";s:13:"mod1/conf.php";s:4:"691e";s:14:"mod1/index.php";s:4:"bdf5";s:18:"mod1/locallang.xml";s:4:"b5bc";s:22:"mod1/locallang_mod.xml";s:4:"db06";s:19:"mod1/moduleicon.gif";s:4:"8074";}',
+    'autoload' => [
+        'psr-4' => array(
+            "Hwt\\HwtImporthandler\\" => "Classes/"
+        )
+    ],
 );
